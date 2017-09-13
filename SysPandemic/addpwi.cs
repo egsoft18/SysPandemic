cb5440138e8d2137f9828eccdd7ca8782fad8e46
edgomez18 <146097@p.uapa.edu.do>
2017-09-12 22:33:31 -0400

    El visor de los procedimientos sin seguros funciona.

--- SysPandemic/addpwi.cs
+++ SysPandemic/addpwi.cs
@@ -47,58 +47,86 @@ namespace SysPandemic
             }
             else
             {
-                SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
-                try
-                {
-                    cnx.Open();
                     string idinsurance = "0";
                     string insurance = "NO SEGURO";
-                    string comando = "INSERT INTO detailsinsurance(pinsurance, tariff, coverage, difference,  idinsurance, insurance) VALUES('" + process_txt.Text + "', '" + priceprocess_txt.Text + "', '00', '"+ priceprocess_txt.Text +"', '" + idinsurance + "', '" + insurance + "');";
-                    SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
-
-                    if (insertion.ExecuteNonQuery() > 0)
-                    {
-                        MessageBox.Show("Se agrego correctamente");
-                        process_txt.Clear();
-                        priceprocess_txt.Clear();
-                        process_txt.Focus();
-                        cnx.Close();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error");
-                }
+                    string query = "INSERT INTO detailsinsurance(pinsurance, tariff, coverage, difference,  idinsurance, insurance) VALUES('" + process_txt.Text + "', '" + priceprocess_txt.Text + "', '00', '" + priceprocess_txt.Text + "', '" + idinsurance + "', '" + insurance + "');";
+                    DBManager c = new DBManager();
+                    c.command(query);
+                    process_txt.Clear();
+                    priceprocess_txt.Clear();
+                    process_txt.Focus();
+                       
             }
+        
+
+
+
+            //if (process_txt.Text.Length < 1)
+            //{
+            //    MessageBox.Show("Debe de introducir la informacion requerida.", "Error");
+            //}
+            //else
+            //{
+            //    SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
+            //    try
+            //    {
+            //        cnx.Open();
+            //        string idinsurance = "0";
+            //        string insurance = "NO SEGURO";
+            //        string comando = "INSERT INTO detailsinsurance(pinsurance, tariff, coverage, difference,  idinsurance, insurance) VALUES('" + process_txt.Text + "', '" + priceprocess_txt.Text + "', '00', '"+ priceprocess_txt.Text +"', '" + idinsurance + "', '" + insurance + "');";
+            //        SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
+
+            //        if (insertion.ExecuteNonQuery() > 0)
+            //        {
+            //            MessageBox.Show("Se agrego correctamente");
+            //            process_txt.Clear();
+            //            priceprocess_txt.Clear();
+            //            process_txt.Focus();
+            //            cnx.Close();
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show(ex.Message, "Error");
+            //    }
+            //}
         
     }
 
         private void updatepwi_btn_Click(object sender, EventArgs e)
         {
-            try
-            {
-                SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
-                cnx.Open();
-                string comando = "UPDATE detailsinsurance set pinsurance = '" + process_txt.Text + "', tariff = '" + priceprocess_txt.Text + "' where id= '" +idprocess_txt.Text+"'";
-                SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
-                if (insertion.ExecuteNonQuery() > 0)
-                {
-                    cnx.Close();
-                    seepwi frm = new seepwi();
+            
+                string query = "UPDATE detailsinsurance set pinsurance = '" + process_txt.Text + "', tariff = '" + priceprocess_txt.Text + "' where id= '" + idprocess_txt.Text + "'";
+                DBManager c = new DBManager();
+                c.command(query);
+                seepwi frm = new seepwi();
                     frm.loaddgv();
-                    MessageBox.Show("Se ha actualizado!");
                     Close();
+
+            //try
+            //{
+            //    SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
+            //    cnx.Open();
+            //    string comando = "UPDATE detailsinsurance set pinsurance = '" + process_txt.Text + "', tariff = '" + priceprocess_txt.Text + "' where id= '" +idprocess_txt.Text+"'";
+            //    SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
+            //    if (insertion.ExecuteNonQuery() > 0)
+            //    {
+            //        cnx.Close();
+            //        seepwi frm = new seepwi();
+            //        frm.loaddgv();
+            //        MessageBox.Show("Se ha actualizado!");
+            //        Close();
                    
-                }
-                else
-                {
-                    MessageBox.Show("No se consiguio");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error");
-            }
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("No se consiguio");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Error");
+            //}
         }
 
         private void deletepwi_btn_Click(object sender, EventArgs e)
@@ -107,31 +135,47 @@ namespace SysPandemic
 
             if (result == DialogResult.Yes)
             {
-                SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
-                try
-                {
-                    cnx.Open();
-                    string comando = "DELETE FROM detailsinsurance WHERE id = '" + idprocess_txt.Text + "'";
-                    SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
-                    if (insertion.ExecuteNonQuery() > 0)
-                    {
-                        MessageBox.Show("Se ha eliminado!");
-                        Close();
-                    }
-                    else
-                    {
-                        MessageBox.Show("Algo fue mal");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error");
-                }
-
+                
+                    string query = "DELETE FROM detailsinsurance WHERE id = '" + idprocess_txt.Text + "'";
+                    DBManager c = new DBManager();
+                    c.command(query);
+                    Close();
             }
             else if (result == DialogResult.No)
             {
             }
+
+
+
+            //DialogResult result = MessageBox.Show("Seguro que desea eliminar este proceso?", "Eliminar proceso", MessageBoxButtons.YesNo);
+
+            //if (result == DialogResult.Yes)
+            //{
+            //    SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
+            //    try
+            //    {
+            //        cnx.Open();
+            //        string comando = "DELETE FROM detailsinsurance WHERE id = '" + idprocess_txt.Text + "'";
+            //        SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
+            //        if (insertion.ExecuteNonQuery() > 0)
+            //        {
+            //            MessageBox.Show("Se ha eliminado!");
+            //            Close();
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("Algo fue mal");
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show(ex.Message, "Error");
+            //    }
+
+            //}
+            //else if (result == DialogResult.No)
+            //{
+            //}
         }
 
         private void addpwi_FormClosing(object sender, FormClosingEventArgs e)
