78fcf3789d1951a6e139351c1d115bd7fed4a25f
edgomez18 <146097@p.uapa.edu.do>
2017-09-15 01:07:32 -0400

    Se han hecho unos cambios minimos.

--- SysPandemic/agresitas.cs
+++ SysPandemic/agresitas.cs
@@ -20,87 +20,100 @@ namespace SysPandemic
 
         private void agrescitas_Load(object sender, EventArgs e)
         {
-            SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
-            try
-            {
-                cnx.Open();
-                SQLiteDataAdapter adac = new SQLiteDataAdapter("Select id as ID, name as Nombre, sex as Sexo, idperson as Cedula from patient", cnx);
-                DataTable tabla = new DataTable("Pacientes");
-                adac.Fill(tabla);
-                dataGridView1.DataSource = tabla;
-
-                SQLiteDataAdapter adac2 = new SQLiteDataAdapter("Select iddoctors as ID, name as Nombre, sex as Sexo, idperson as Cedula from doctors", cnx);
-                DataTable tabla2 = new DataTable("doctors");
-                adac2.Fill(tabla2);
-                dataGridView2.DataSource = tabla2;
-
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            string query = "Select idpatient as ID, name as Nombre, sex as Sexo, idperson as Cedula from patient";
+            DBManager c = new DBManager();
+            c.load_dgv(dataGridView1, query);
+            
+            query = "Select idstaff as ID, namestaff as Nombre, sexstaff as Sexo, idpersonstaff as Cedula from staff";
+            c.load_dgv(dataGridView2, query);
+               
         }
 
         private void btnguardar_Click(object sender, EventArgs e)
         {
-            SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
-            try
-            {
-                cnx.Open();
-                string comando = "INSERT INTO datem(idpatient,namepa,iddoctor,namedoctor,assist,date,time) values('" + txtidpa.Text + "','" + txtpaciente.Text + "','" + txtiddo.Text + "','" + txtdoct.Text + "','Pendiente','" + dtpfecha.Text + "','" + dtphora.Text + "')";
-                SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
+            string query = "INSERT INTO datem(idpatient,namepa,iddoctor,namedoctor,assist,date,time) values('" + txtidpa.Text + "','" + txtpaciente.Text + "','" + txtiddo.Text + "','" + txtdoct.Text + "','Pendiente','" + dtpfecha.Text + "','" + dtphora.Text + "')";
+            DBManager c = new DBManager();
+            c.command(query);
+            txtidpa.Clear();
+            txtpaciente.Clear();
+            txtdoct.Clear();
+            txtiddo.Clear();
 
 
-                if (insertion.ExecuteNonQuery() > 0)
-                {
-                    MessageBox.Show("Se agrego correctamente");
-                    txtidpa.Clear();
-                    txtpaciente.Clear();
-                    txtdoct.Clear();
-                    txtiddo.Clear();
-                }
+
+            //SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
+            //try
+            //{
+            //    cnx.Open();
+            //    string comando = "INSERT INTO datem(idpatient,namepa,iddoctor,namedoctor,assist,date,time) values('" + txtidpa.Text + "','" + txtpaciente.Text + "','" + txtiddo.Text + "','" + txtdoct.Text + "','Pendiente','" + dtpfecha.Text + "','" + dtphora.Text + "')";
+            //    SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
 
 
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            //    if (insertion.ExecuteNonQuery() > 0)
+            //    {
+            //        MessageBox.Show("Se agrego correctamente");
+            //        txtidpa.Clear();
+            //        txtpaciente.Clear();
+            //        txtdoct.Clear();
+            //        txtiddo.Clear();
+            //    }
+
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
-            try
-            {
-                cnx.Open();
                 DialogResult result = MessageBox.Show("Seguro que desea Actualizar?", "Actualizar datos del Paciente", MessageBoxButtons.YesNo);
                 if (result == DialogResult.Yes)
                 {
-                    string comando = "UPDATE datem  set idpatient ='" + txtidpa.Text + "', namepa ='" + txtpaciente.Text + "', iddoctor = '" + txtiddo.Text + "', namedoctor = '" + txtdoct.Text + "', date = '" + dtpfecha.Text + "', time = '" + dtphora.Text + "' where id = '" + txtid.Text + "'";
-                    SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
-                    if (insertion.ExecuteNonQuery() > 0)
-                    {
-                        MessageBox.Show("Se ha actualizado!");
-                        Close();
-                    }
-                    else
-                    {
-                        MessageBox.Show("Algo fallo!!!");
-                    }
+                    string query = "UPDATE datem  set idpatient ='" + txtidpa.Text + "', namepa ='" + txtpaciente.Text + "', iddoctor = '" + txtiddo.Text + "', namedoctor = '" + txtdoct.Text + "', date = '" + dtpfecha.Text + "', time = '" + dtphora.Text + "' where id = '" + txtid.Text + "'";
+                    DBManager c = new DBManager();
+                    c.command(query);
                 }
                 else if (result == DialogResult.No)
                 {
 
                 }
 
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error");
+            
 
-            }
+
+
+            //SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
+            //try
+            //{
+            //    cnx.Open();
+            //    DialogResult result = MessageBox.Show("Seguro que desea Actualizar?", "Actualizar datos del Paciente", MessageBoxButtons.YesNo);
+            //    if (result == DialogResult.Yes)
+            //    {
+            //        string comando = "UPDATE datem  set idpatient ='" + txtidpa.Text + "', namepa ='" + txtpaciente.Text + "', iddoctor = '" + txtiddo.Text + "', namedoctor = '" + txtdoct.Text + "', date = '" + dtpfecha.Text + "', time = '" + dtphora.Text + "' where id = '" + txtid.Text + "'";
+            //        SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
+            //        if (insertion.ExecuteNonQuery() > 0)
+            //        {
+            //            MessageBox.Show("Se ha actualizado!");
+            //            Close();
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("Algo fallo!!!");
+            //        }
+            //    }
+            //    else if (result == DialogResult.No)
+            //    {
+
+            //    }
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Error");
+
+            //}
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -129,18 +142,10 @@ namespace SysPandemic
         }
         private void buscard()
         {
-            SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
-            try
-            {
-                SQLiteDataAdapter adac = new SQLiteDataAdapter("Select iddoctors as ID, name as Nombre, sex as Sexo, idperson as Cedula from doctors where name like '%" + txtbuscardoc.Text + "%'", cnx);
-                DataTable tabla = new DataTable("Doctores");
-                adac.Fill(tabla);
-                dataGridView2.DataSource = tabla;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            string query = "Select idstaff as ID, namestaff as Nombre, sexstaff as Sexo, idpersonstaff as Cedula from staff where namestaff like '%" + txtbuscardoc.Text + "%'";
+            DBManager c = new DBManager();
+            c.load_dgv(dataGridView2, query);
+               
         }
 
         private void btnborrar_Click(object sender, EventArgs e)
@@ -149,54 +154,59 @@ namespace SysPandemic
 
             if (result == DialogResult.Yes)
             {
-                SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
-                try
-                {
-                    cnx.Open();
-                    string comando = "DELETE FROM datem WHERE id = '" + txtid.Text + "'";
-                    SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
-                    if (insertion.ExecuteNonQuery() > 0)
-                    {
-                        MessageBox.Show("Se ha eliminado!");
-                        Close();
-                    }
-                    else
-                    {
-                        MessageBox.Show("Algo fue mal");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error");
-                }
+                    string query = "DELETE FROM datem WHERE id = '" + txtid.Text + "'";
+                    DBManager c = new DBManager();
+                    c.command(query);
+                    Close();
+                    
 
             }
             else if (result == DialogResult.No)
             {
             }
+
+
+
+            //DialogResult result = MessageBox.Show("Seguro que desea eliminar esta Cita?", "Eliminar Cita", MessageBoxButtons.YesNo);
+
+            //if (result == DialogResult.Yes)
+            //{
+            //    SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
+            //    try
+            //    {
+            //        cnx.Open();
+            //        string comando = "DELETE FROM datem WHERE id = '" + txtid.Text + "'";
+            //        SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
+            //        if (insertion.ExecuteNonQuery() > 0)
+            //        {
+            //            MessageBox.Show("Se ha eliminado!");
+            //            Close();
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("Algo fue mal");
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show(ex.Message, "Error");
+            //    }
+
+            //}
+            //else if (result == DialogResult.No)
+            //{
+            //}
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
-            try
-            {
-                cnx.Open();
                 string asis = "Asistio";
-                string comando = "UPDATE datem  set assist = '" + asis + "' where id = '" + txtid.Text + "'";
-                SQLiteCommand insertion = new SQLiteCommand(comando, cnx);
-                if (insertion.ExecuteNonQuery() > 0)
-                {
-                    MessageBox.Show("Se ha actualizado!");
-                    Close();
-                }
+                string query = "UPDATE datem  set assist = '" + asis + "' where id = '" + txtid.Text + "'";
+                DBManager c = new DBManager();
+                c.command(query);
 
+                
 
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
         }
 
         private void btncanc_Click(object sender, EventArgs e)
@@ -210,19 +220,10 @@ namespace SysPandemic
         }
         private void buscarp()
         {
-            SQLiteConnection cnx = new SQLiteConnection("Data Source=C:\\syspandemic\\db\\syspandemic.db;Version=3;");
-            try
-            {
-                cnx.Open();
-                SQLiteDataAdapter adac = new SQLiteDataAdapter("Select id as ID, name as Nombre, sex as Sexo, idperson as Cedula from patient where name like '%" + txtbuscarp.Text + "%'", cnx);
-                DataTable tabla = new DataTable("Pacientes");
-                adac.Fill(tabla);
-                dataGridView1.DataSource = tabla;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            string query = "Select idpatient as ID, name as Nombre, sex as Sexo, idperson as Cedula from patient where name like '%" + txtbuscarp.Text + "%'";
+            DBManager c = new DBManager();
+            c.load_dgv(dataGridView1, query);
+                
         }
 
         private void txtbuscardoc_TextChanged(object sender, EventArgs e)
